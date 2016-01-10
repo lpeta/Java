5b23dcc882c577df6ed19d345b1e89bb1e38150d
Leonardo Peta <leonardopeta@live.co.uk>
2016-01-10 18:23:19 +0000

    new exercicies

---
+++ chapter_01/exercises/Quilo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Quilo
+{
+  public static void main(String[] args) {
+    System.out.println("This program will transform from pounds to kilo");
+    double p = 0;
+    double k = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value in pounds:    ");
+    p = sc.nextDouble();
+    k = p/2.2;
+    System.out.println("The value in Kilos is " + k);
+
+    }
+}
