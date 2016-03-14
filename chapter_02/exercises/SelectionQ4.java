faeacf1964862f0282a274447ece7e193177121d
Leonardo Peta <leonardopeta@live.co.uk>
2016-03-14 21:14:18 +0000

    update exercicies on chapter 02

---
+++ chapter_02/exercises/SelectionQ4.java
@@ -0,0 +1,14 @@
+import java.util. *;
+public class SelectionQ4 {
+  public static void main(String[] args) {
+    int x;
+    Scanner sc = new Scanner(System.in);
+    System.out.print ("Enter a number: ");
+    x = sc.nextInt();
+    if (x > 10) {
+      System.out.println("Green");
+      System.out.println("Blue");
+    }
+    System.out.println("RED");
+  }
+}
