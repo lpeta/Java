117a073ccf2327aecdd3b155ecaf567f6f339d98
Leonardo Peta <leonardopeta@live.co.uk>
2016-04-02 22:54:42 +0100

    creating new file tutorial

---
+++ chapter_03/exercises/InterationQ3.java
@@ -0,0 +1,19 @@
+/**
+ * InterationQ3 calculating the value of a inter and print it only if is a
+ * whole number
+ *
+ */
+public class InterationQ3 {
+
+  public static void main(String[] args) {
+
+    for(int i = 1; i <= 10; i++) {
+
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+
+  }
+
+}
