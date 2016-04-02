3d40d7abad3f2dd14f26a3e621f50066372e9a6f
Leonardo Peta <leonardopeta@live.co.uk>
2016-04-02 22:45:16 +0100

    creating new file tutorial

---
+++ chapter_03/exercises/InterationQ2.java
@@ -0,0 +1,23 @@
+/**
+ * Interation compare the value imput by user and display message
+ * after loop interation
+ *
+ */
+
+import java.util.*;
+
+public class InterationQ2 {
+    public static void main(String[] args) {
+
+      int num;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a number  ");
+      num = sc.nextInt();
+
+      for(int i = 1; i < num; i++) {
+        System.out.println("YES");
+        System.out.println("NO");
+      }
+      System.out.println("OK");
+    }
+}
